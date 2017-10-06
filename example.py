b2ae523f88f558f76ee702d04bad67417b9f5ce3
Z Lee <jzuriel85@gmail.com>
2017-10-06 13:39:16 -0400

    Create example.py

---
+++ example.py
@@ -0,0 +1,13 @@
+import time
+
+print("Welcome")
+time.sleep(2)
+print("What is your name?")
+name = input()
+if name != "":
+  print("Hello, " + name)
+  print("It is nice to meet you.")
+else:
+  print("I'm sorry. I did not catch your name.")
+print("Thank you for trying out this sample code.")
+print("Check my page later for more.")
